a174fa0dcbd247a553fe4c004ddc84fc4c508e86
Christian Acosta <christianjavieracosta@outlook.com>
2026-09-07 11:12:18 -0400

    Create programa.cpp

---
+++ programa.cpp
@@ -0,0 +1,22 @@
+/*
+10/9/26
+Grupo 1:
+- Alessandro Salamone - 1132116
+- Carlos Minaya - 1132836
+- Axel Almonte - 1131078
+- ⁠Octavio Ramírez - 1132995
+- ⁠José Pinales - 1133255
+- Christian Acosta - 1132698
+
+Haga un programa C++ que permita manejar la pista de aterrizaje del aeropuerto de
+Ciudad Gótica, sabiendo que, el programa debe insertar (encolar = Enqueue) los vuelos
+en la cola priorizada según se describió anteriormente.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  return 0;
+}
