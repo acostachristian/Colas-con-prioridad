f7efad98a757e94d213839273aee304321244445
Christian Acosta <christianjavieracosta@outlook.com>
2026-09-09 01:08:59 -0400

    Creacion de ContarNodos y Enqueue

--- programa.cpp
+++ programa.cpp
@@ -16,11 +16,89 @@ en la cola priorizada según se describió anteriormente..
 #include <iostream>
 using namespace std;
 
-struct Nodo {
-int vuelo;
-int prioridad;
-Nodo *next;
+struct Nodo
+{
+  int vuelo;
+  int prioridad;
+  Nodo *next;
 };
+
+int ContarNodos(Nodo *frente)
+{ // para determinar cuantos nodos hay en la cola.
+  int contador = 0;
+  Nodo *actual = frente;
+
+  while (actual != NULL)
+  {
+    contador++;
+    actual = actual->next;
+  }
+
+  return contador;
+}
+
+void Enqueue(Nodo *&frente, Nodo *&fin, int pVuelo, int pPrioridad)
+{ // frente es por donde se sale y fin es por donde se entra
+  Nodo *nuevoNodo = new Nodo();
+
+  nuevoNodo->vuelo = pVuelo;
+  if (pPrioridad > 16)
+  {
+    pPrioridad = 16;
+  }
+  else if (pPrioridad < 0)
+  {
+    pPrioridad = 0;
+  }
+  nuevoNodo->prioridad = pPrioridad;
+
+  //[[Auxiliares para recorrer la cola]]
+  Nodo *nodoActual = frente;
+  Nodo *nodoAnterior = NULL;
+
+  int cantidadNodos = ContarNodos(frente);
+
+  if (frente == NULL)
+  {
+    frente = nuevoNodo;
+    fin = nuevoNodo;
+    nuevoNodo->next = NULL;
+    return;
+  }
+
+  // Se recorre la cola, y se guarda el nodoAnterior a donde se insertara nuevoNodo
+  // y nodoActual sera el que va siguiente a nuevoNodo
+  else if (pPrioridad <= cantidadNodos)
+  {
+    for (int i = 0; i < (pPrioridad - 1); i++)
+    {
+      nodoAnterior = nodoActual;
+      nodoActual = nodoActual->next;
+    }
+
+    if (frente == nodoActual)
+    {
+      frente = nuevoNodo;
+    }
+    else
+    {
+      nodoAnterior->next = nuevoNodo;
+    }
+
+    nuevoNodo->next = nodoActual;
+
+    if (nodoActual == NULL)
+    {
+      fin = nuevoNodo;
+    }
+  }
+  else
+  {
+    fin->next = nuevoNodo;
+    fin = nuevoNodo;
+    nuevoNodo->next = NULL;
+  }
+}
 
 int main()
 {
